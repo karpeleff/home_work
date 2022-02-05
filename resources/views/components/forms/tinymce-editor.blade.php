4877fc2fe07719303f71f9fb691fcbb619b6c58c
Aleksey <alekseykarpelev@gmail.com>
2022-02-05 14:46:43 +1000

    init

--- resources/views/components/forms/tinymce-editor.blade.php
+++ resources/views/components/forms/tinymce-editor.blade.php
@@ -1,7 +1,3 @@
-<div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
-    <form method="post">
-   <textarea id="myeditorinstance">Hello, World!</textarea>
- </form>
 
-</div>
+   <textarea id="myeditorinstance">Hello, World!</textarea>
+
